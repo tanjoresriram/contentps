531378bf46b01681467466797a03559fcae7fbe3
tanjoresriram <tanjore@gmail.com>
2025-06-11 10:03:23 -0400

    upload new version

--- filecreate.ps1
+++ filecreate.ps1
@@ -1,22 +1,4 @@
 # This is a comment in the script
 
 # Get a list of all files in the current directory
-Get-ChildItem
-
-# Get the current date and time
-Get-Date
-
-# Print a message to the console
-Write-Host "Hello, world!"
-
-# Get the version of PowerShell you are using
-Get-Host | Select-Object PSVersion
-
-# Create a new file
-New-Item -Path "C:\temp\newfile.txt" -ItemType File -Force
-
-# Add some content to the new file
-Add-Content -Path "C:\temp\newfile.txt" -Value "This is some sample text."
-
-# Get the contents of the new file
-Get-Content -Path "C:\temp\newfile.txt"
+Invoke-WebRequest -Uri "https://winscp.net/download/WinSCP-6.5.1-Setup.exe/download" -OutFile "C:\temp\setup.exe"
